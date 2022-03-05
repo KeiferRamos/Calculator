5b6df83bd4ed117019a28c902f08000243b027ea
KeiferRamos <keiferramos2001@gmail.com>
2022-03-05 10:15:05 +0800

    afwrw

--- script.js
+++ script.js
@@ -37,17 +37,24 @@ function numberOnClick(e) {
 
 function operatorOnClick(e) {
   const inputs = inputText.innerHTML;
-  if ((isNaN(inputs.slice(-1)) && inputs.slice(-1) != ".") || !inputs) {
+  const lastChar = Array.from(inputs).pop();
+  if (!inputs || (inputs.slice(-1) == "." && isNaN(inputs.slice(-2)))) {
     return;
   } else if (
     inputs.slice(-1) == "." &&
     operators.includes(inputs.charAt(inputs.length - 2))
   ) {
     return;
+  } else if (operators.includes(lastChar)) {
+    inputText.innerHTML = inputs.replace(
+      new RegExp(lastChar + "$"),
+      e.target.innerHTML
+    );
   } else {
     inputText.innerHTML += e.target.innerHTML;
   }
 }
+// inputs.slice(-1) != "." ||
 
 function deleteOnclick() {
   const inputs = inputText.innerHTML;
@@ -77,76 +84,92 @@ function clearOnClick() {
 }
 
 function answerOnClick() {
-  inputText.innerHTML = answer.innerHTML;
+  if (answer.innerHTML == "Error") {
+    inputText.innerHTML = 0;
+  } else {
+    inputText.innerHTML = answer.innerHTML;
+  }
 }
 
 function equalsOnClick() {
-  const input = inputText.value;
-  let n = input.split(/[−,+,÷,×]+/);
-  const operator = [];
-  Array.from(input).forEach((el) => {
-    if (operators.includes(el)) {
-      operator.push(el);
+  if (operators.includes(Array.from(inputText.innerHTML).pop())) {
+    return;
+  } else {
+    const input = inputText.value;
+    let n = input.split(/[−,+,÷,×]+/);
+    const operator = [];
+    Array.from(input).forEach((el) => {
+      if (operators.includes(el)) {
+        operator.push(el);
+      }
+    });
+    let i1 = 0;
+    let i2 = 1;
+    let notDone = false;
+    while (i1 < operator.length) {
+      n.splice(i2, 0, operator[i1]);
+      i1++;
+      i2 += 2;
     }
-  });
-
-  let i1 = 0;
-  let i2 = 1;
-  let notDone = false;
-  while (i1 < operator.length) {
-    n.splice(i2, 0, operator[i1]);
-    i1++;
-    i2 += 2;
-  }
-  n = n.map((el) => {
-    return { count: el, id: Math.random() };
-  });
-
-  while (!notDone) {
-    if (!n.some((el) => operator.includes(el.count))) {
-      notDone = true;
+    n = n.map((el) => {
+      return { count: el, id: Math.random() };
+    });
+    console.log(n[n.length - 1]);
+    if (operators.includes(n[n.length - 1])) {
+      return;
     }
-    let num1, num2;
-    for (var i = 0; i < n.length; i++) {
-      if (n.some((el) => el.count == "×" || el.count == "÷")) {
-        if (n[i].count == "×" || n[i].count == "÷") {
-          num1 = n[i - 1].count;
-          num2 = n[i + 1].count;
-          if (n[i].count === "×") {
-            n.splice(n.indexOf(n[i - 1]), 3, {
-              count: num1 * num2,
-              id: Math.random(),
-            });
-            break;
-          } else {
-            n.splice(n.indexOf(n[i - 1]), 3, {
-              count: num1 / num2,
-              id: Math.random(),
-            });
-            break;
+    while (!notDone) {
+      if (!n.some((el) => operator.includes(el.count))) {
+        notDone = true;
+      }
+      let num1, num2;
+      for (var i = 0; i < n.length; i++) {
+        if (n.some((el) => el.count == "×" || el.count == "÷")) {
+          if (n[i].count == "×" || n[i].count == "÷") {
+            num1 = n[i - 1].count;
+            num2 = n[i + 1].count;
+            if (n[i].count === "×") {
+              n.splice(n.indexOf(n[i - 1]), 3, {
+                count: num1 * num2,
+                id: Math.random(),
+              });
+              break;
+            } else {
+              n.splice(n.indexOf(n[i - 1]), 3, {
+                count: num1 / num2,
+                id: Math.random(),
+              });
+              break;
+            }
           }
-        }
-      } else if (!n.some((el) => el.count == "×" || el.count == "÷")) {
-        if (n[i].count == "−" || n[i].count == "+") {
-          num1 = +n[i - 1].count;
-          num2 = +n[i + 1].count;
-          if (n[i].count == "+") {
-            n.splice(n.indexOf(n[i - 1]), 3, {
-              count: num1 + num2,
-              id: Math.random(),
-            });
-            break;
-          } else if (n[i].count == "−") {
-            n.splice(n.indexOf(n[i - 1]), 3, {
-              count: num1 - num2,
-              id: Math.random(),
-            });
-            console.log([...n]);
-            break;
+        } else {
+          if (n[i].count == "−" || n[i].count == "+") {
+            num1 = +n[i - 1].count;
+            num2 = +n[i + 1].count;
+            if (n[i].count == "+") {
+              n.splice(n.indexOf(n[i - 1]), 3, {
+                count: num1 + num2,
+                id: Math.random(),
+              });
+              break;
+            } else if (n[i].count == "−") {
+              n.splice(n.indexOf(n[i - 1]), 3, {
+                count: num1 - num2,
+                id: Math.random(),
+              });
+              console.log([...n]);
+              break;
+            }
           }
         }
       }
     }
+    if (n[0].count == "Infinity" || n[0].count == "NaN") {
+      answer.innerHTML = "Error";
+    } else if (`${n[0].count}`.includes(".") && `${n[0].count}`.length > 5) {
+      answer.innerHTML = +n[0].count.toFixed(1);
+    } else {
+      answer.innerHTML = n[0].count;
+    }
   }
-  answer.innerHTML = n[0].count;
 }
